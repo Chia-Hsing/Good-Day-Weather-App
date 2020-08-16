abea658ca44b883c54b93f18bbd6ff8fc5a643f0
ausgeflippte <ausgeflippte@gmail.com>
2020-08-16 20:07:04 +0800

    Updates

---
+++ precache-manifest.d0329f29b4dd034a0e1f9b24ad399cc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b663abf4301dbbdca5e802e5c16471bd",
+    "url": "/Good-Day-Weather-App-react/index.html"
+  },
+  {
+    "revision": "0ae73c4eb9bfe6e9e5bf",
+    "url": "/Good-Day-Weather-App-react/static/css/main.21c9b2ed.chunk.css"
+  },
+  {
+    "revision": "0f5125486868770fa99d",
+    "url": "/Good-Day-Weather-App-react/static/js/2.249d7ac5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Good-Day-Weather-App-react/static/js/2.249d7ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae73c4eb9bfe6e9e5bf",
+    "url": "/Good-Day-Weather-App-react/static/js/main.7f616bdc.chunk.js"
+  },
+  {
+    "revision": "8c0b3e6a29fb1d4a9647",
+    "url": "/Good-Day-Weather-App-react/static/js/runtime-main.2114ef84.js"
+  }
+]);
